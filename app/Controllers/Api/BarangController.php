891aa51222069ca90240d48d91fb5f5fb2cfefc2
bahrul62 <iruliyus00@gmail.com>
2022-06-12 01:46:40 +0700

    restapi dan crud

---
+++ app/Controllers/Api/BarangController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use CodeIgniter\RESTful\ResourceController;
+use App\Models\BarangModel;
+
+class BarangController extends ResourceController
+{
+    /**
+     * Return an array of resource objects, themselves in array format
+     *
+     * @return mixed
+     */
+    public function index()
+    {
+        $model = new BarangModel;
+        return $this->respond($model->findAll());
+    }
+
+    /**
+     * Return the properties of a resource object
+     *
+     * @return mixed
+     */
+    public function show($id = null)
+    {
+        //
+    }
+
+    /**
+     * Return a new resource object, with default properties
+     *
+     * @return mixed
+     */
+    public function new()
+    {
+        //
+    }
+
+    /**
+     * Create a new resource object, from "posted" parameters
+     *
+     * @return mixed
+     */
+    public function create()
+    {
+        $model = new BarangModel;
+
+        $model->insert([
+            'nama_barang' => $this->request->getPost('nama_barang'),
+            'satuan' => $this->request->getPost('satuan'),
+            'stok_barang' => $this->request->getPost('stok_barang')
+        ]);
+    }
+
+    /**
+     * Return the editable properties of a resource object
+     *
+     * @return mixed
+     */
+    public function edit($id = null)
+    {
+        $model = new BarangModel;
+
+        $model->update($id, [
+            'nama_barang' => $this->request->getPost('nama_barang'),
+            'satuan' => $this->request->getPost('satuan'),
+            'stok_barang' => $this->request->getPost('stok_barang')
+        ]);
+    }
+
+    /**
+     * Add or update a model resource, from "posted" properties
+     *
+     * @return mixed
+     */
+    public function update($id = null)
+    {
+        $model = new BarangModel;
+
+        $model->update($id, [
+            'nama_barang' => $this->request->getPost('nama_barang'),
+            'satuan' => $this->request->getPost('satuan'),
+            'stok_barang' => $this->request->getPost('stok_barang')
+        ]);
+    }
+
+    /**
+     * Delete the designated resource object from the model
+     *
+     * @return mixed
+     */
+    public function delete($id = null)
+    {
+        $model = new BarangModel;
+        $model->delete($id);
+    }
+}
